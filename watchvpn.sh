5f06f52bcbc45d61a9c8f1a6f8ec1d248fb47e96
Ubuntu <ubuntu@ip-172-31-43-146.us-west-2.compute.internal>
2017-08-24 00:59:49 +0000

    Instance edits

--- watchvpn.sh
+++ watchvpn.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
-
 # parameters
 WATCHCONNECT=llang-pptp
 WATCHINTERVAL=15
+
+# startup
+echo "Starting watchvpn"
+echo "Starting VPN"
+sudo pppd call $WATCHCONNECT
+sleep $WATCHINTERVAL
+
 
 # repeat until interrupted
 while true
@@ -13,6 +19,7 @@ do
 # http://www.cyberciti.biz/tips/howto-configure-ubuntu-fedora-linux-pptp-client.html
 if ! ifconfig | grep -q ppp0
 then
+	echo "Restarting VPN"
 	sudo killall pppd
 	sudo pppd call $WATCHCONNECT
 	sleep 10
